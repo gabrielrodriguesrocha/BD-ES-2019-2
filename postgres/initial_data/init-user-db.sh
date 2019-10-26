6aa8345a9f12e701aed6dd0fe9ca686da6c99a3c
Gabriel Rodrigues Rocha <gabrielrodrigues_rocha@hotmail.com>
2019-10-25 22:26:42 -0300

    Added login schema and table creation scripts

--- postgres/initial_data/init-user-db.sh
+++ postgres/initial_data/init-user-db.sh
@@ -3,4 +3,24 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE exams;
+	CREATE SCHEMA internal
+    	AUTHORIZATION postgres;
+	-- Table: internal.users
+
+	-- DROP TABLE internal.users;
+	
+	CREATE TABLE internal.users
+	(
+	    username character varying(25) COLLATE pg_catalog."default" NOT NULL,
+	    password character varying(255) COLLATE pg_catalog."default",
+	    employee boolean,
+	    CONSTRAINT users_pkey PRIMARY KEY (username)
+	)
+	WITH (
+	    OIDS = FALSE
+	)
+	TABLESPACE pg_default;
+	
+	ALTER TABLE internal.users
+	    OWNER to postgres;
 EOSQL
