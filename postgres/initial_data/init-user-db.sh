f58f70c198d7a74b56c9c2b1d9b07ad722d776a6
Gabriel Rodrigues Rocha <gabrielrodrigues_rocha@outlook.com>
2019-10-28 21:38:38 -0300

    Dividing internal and public db creation scripts

--- postgres/initial_data/init-user-db.sh
+++ postgres/initial_data/init-user-db.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE DATABASE exams;
-	CREATE SCHEMA internal
-    	AUTHORIZATION postgres;
-	-- Table: internal.users
-
-	-- DROP TABLE internal.users;
-	
-	CREATE TABLE internal.users
-	(
-	    username character varying(25) COLLATE pg_catalog."default" NOT NULL,
-	    password character varying(255) COLLATE pg_catalog."default",
-	    employee boolean,
-	    CONSTRAINT users_pkey PRIMARY KEY (username)
-	)
-	WITH (
-	    OIDS = FALSE
-	)
-	TABLESPACE pg_default;
-	
-	ALTER TABLE internal.users
-	    OWNER to postgres;
-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f create-internal.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f create-db.sql
